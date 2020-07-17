199993d61e9bccb895422cfae6b9cc7286e7f40b
Maxi Paolucci mac <maxipaolucci@gmail.com>
2020-07-17 16:42:42 +1200

    more smart pointers

--- game.cpp
+++ game.cpp
@@ -1,23 +1,22 @@
 #include "game.h"
 
-Game::Game(Board *board) : board{board}
+Game::Game()
 {
   this->new_game();
 }
 
-Game::Game(const Game &source) : Game{source.board} {
-  cout << "Game copy constructor called."<< endl;
-}
+// Game::Game(const Game &source) : Game{source.board_ptr} {
+//   cout << "Game copy constructor called."<< endl;
+// }
 
-Game::Game(Game &&source) : Game{source.board} {
-  source.board = nullptr;
-  cout << "Game move constructor called."<< endl;
-}
+// Game::Game(Game &&source) : Game{source.board} {
+//   source.board = nullptr;
+//   cout << "Game move constructor called."<< endl;
+// }
 
 Game::~Game()
 {
   cout << "Destroying Game object" << endl;
-  this->end_game();
 }
 
 unique_ptr<Player> Game::get_player(char shape) {
@@ -35,7 +34,7 @@ void Game::create_players() {
 }
 
 void Game::create_board() {
-  this->board = new Board();
+  this->board_ptr = make_unique<Board>();
 }
 
 void Game::new_game() {
@@ -43,18 +42,13 @@ void Game::new_game() {
   
   ScoreBoard::create_instance(this->player_one, this->player_two);
 
-  if (this->board == nullptr) {
+  if (!this->board_ptr) {
     this->create_board();
   }
 
-  ConsoleRenderer::get_instance()->render_board(this->board->get_board());
+  ConsoleRenderer::get_instance()->render_board(this->board_ptr->get_board());
 }
 
-Board *Game::get_board() const {
-  return this->board;
+unique_ptr<Board> &Game::get_board() {
+  return this->board_ptr;
 }
-
-void Game::end_game() {
-  delete this->board;
-}
-
--- game.h
+++ game.h
@@ -16,20 +16,19 @@ class Game
 private:
   Player player_one;
   Player player_two;
-  Board *board;
+  unique_ptr<Board> board_ptr;
   
 public:
-  Game(Board *board = nullptr);
-  Game(const Game &source); //copy constructor. Called if move constructor is not provided  (l-values: named values Game game{} )
-  Game(Game &&source); //move constructor . (r-values: temp/unnamed. e.g.: Player *p = *(Game{}).get_player('x'); )
+  Game();
+  // Game(const Game &source); //copy constructor. Called if move constructor is not provided  (l-values: named values Game game{} )
+  // Game(Game &&source); //move constructor . (r-values: temp/unnamed. e.g.: Player *p = *(Game{}).get_player('x'); )
   ~Game();
   
   unique_ptr<Player> get_player(char shape);
   void create_players();
   void create_board();
   void new_game();
-  void end_game();
-  Board *get_board() const;
+  unique_ptr<Board> &get_board(); //as it is a unique_ptr we need to return a reference otherwise it should be a share_ptr to make a copy
 };
 
 #endif // GAME_H_
